eb380e91a47623dcfbb7d6c566313ad893639e8f
mensheviks <mensheviks@126.com>
2017-08-09 23:40:50 +0800

    Add json to read data

--- run.py
+++ run.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	jsonfile = open('static/data/index.json', 'r+')
+	jsontext = jsonfile.read()
+	data = json.loads(jsontext)
+	print data
+	return render_template('index.html', data = data)
 
 
 if __name__ == '__main__':
